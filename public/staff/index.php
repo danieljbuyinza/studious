bfdd24051cc0f35d7385c4da58e03b3bad225b9e
Daniel Buyinza <danieljbuyinza@gmail.com>
2023-11-09 15:03:37 +0300

    Create and style the first page

---
+++ public/staff/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Studious</title>
+  <link rel="stylesheet" href="../styles/staff.css">
+</head>
+<body>
+  <header>
+    <h1>Studious - Staff Area</h1>
+  </header>
+
+  <nav>
+    <ul>
+      <li><a href="index.php">Menu</a></li>
+    </ul>
+  </nav>
+
+  <div id="content">
+
+  </div>
+
+  <footer>
+    &copy; <?php echo date('Y'); ?> Studious
+  </footer>
+</body>
+</html>
